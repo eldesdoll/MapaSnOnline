a088809c2ccd801c3197d80d43f48e34bdce4de4
eldesdoll <38787939+eldesdoll@users.noreply.github.com>
2019-04-15 13:51:25 -0500

    Online
    Tiene algunos fallos en la escena del mapa

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject canvas;
+    public GameObject scenenCam;
+
+    public void Awake()
+    {
+        canvas.SetActive(true);
+    }
+
+    public void SpawnPlayer()
+    {
+        float randomValue = Random.Range(-5, 5);
+        PhotonNetwork.Instantiate(playerPrefab.name, 
+            new Vector2(playerPrefab.transform.position.x * randomValue, 
+            playerPrefab.transform.position.y), Quaternion.identity,0);  
+
+        canvas.SetActive(false);
+        scenenCam.SetActive(false);
+    } 
+}
