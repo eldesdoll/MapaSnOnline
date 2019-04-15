bc66dd810f2c1fbf1cf7d43375d6a9f25868b4bb
isaigalvan <isaig2015@gmail.com>
2019-04-15 13:46:14 -0500

    MOVIMIENTO DE PERSONAJES, BUG REPARADO DE ZOREM, NO USAR MAS DE 1 HABILIDAD, CAMINAR AUSTIN

---
+++ Assets/Scripts/CrearPersonaje.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CrearPersonaje : MonoBehaviour
+{
+    public float posx, posy, posx2, posy2;
+    public int idPersonaje,total;
+    public GameObject personajePrefab, per;
+    public Transform personajeParent;
+    public Sprite[] sprites;
+    public GameObject IconoPrefab;
+    public Transform IconoParent;
+    public List<GameObject> iconos = new List<GameObject>();
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Crear();
+        crearIconos();
+    }
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Crear()
+    {
+        posy = 4.5f; posx = 5;
+        AsignarCoord();
+        GameObject personajeTemp = Instantiate(personajePrefab, new Vector3(posx, posy, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
+        personajeTemp.transform.parent = personajeParent;
+        per = GameObject.FindGameObjectWithTag("PerPref");
+        per.GetComponent<Personaje>().AsignarTamanos();
+        AsignarTexturas();  
+    }
+    public void crearIconos()
+    {
+        total = 3;
+        posx2 = 2.5f;
+        posy2 = 1.3f;
+        for (int i = 0; i < total; i++)
+        {
+
+            GameObject iconoTemp = Instantiate(IconoPrefab, new Vector3(posx2, posy2, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
+            iconos.Add(iconoTemp);
+            iconoTemp.transform.parent = IconoParent;
+            posx2 = posx2 + 2;
+        }
+        iconosPersonajes();
+    }
+    public void iconosPersonajes()
+    {
+        switch (idPersonaje)
+        {
+            case 0:
+                iconos[0].GetComponent<Icono>().AsignarTextura(sprites[0]);
+                iconos[1].GetComponent<Icono>().AsignarTextura(sprites[1]);
+                iconos[2].GetComponent<Icono>().AsignarTextura(sprites[2]);
+                break;
+            case 1:
+                iconos[0].GetComponent<Icono>().AsignarTextura(sprites[3]);
+                iconos[1].GetComponent<Icono>().AsignarTextura(sprites[4]);
+                iconos[2].GetComponent<Icono>().AsignarTextura(sprites[5]);
+                break;
+            case 2:
+                iconos[0].GetComponent<Icono>().AsignarTextura(sprites[6]);
+                iconos[1].GetComponent<Icono>().AsignarTextura(sprites[7]);
+                iconos[2].GetComponent<Icono>().AsignarTextura(sprites[8]);
+                break;
+            case 3:
+                iconos[0].GetComponent<Icono>().AsignarTextura(sprites[9]);
+                iconos[1].GetComponent<Icono>().AsignarTextura(sprites[10]);
+                iconos[2].GetComponent<Icono>().AsignarTextura(sprites[11]);
+                break;
+            case 4:
+                iconos[0].GetComponent<Icono>().AsignarTextura(sprites[12]);
+                iconos[1].GetComponent<Icono>().AsignarTextura(sprites[13]);
+                iconos[2].GetComponent<Icono>().AsignarTextura(sprites[14]);
+                break;
+            case 5:
+                iconos[0].GetComponent<Icono>().AsignarTextura(sprites[3]);
+                iconos[1].GetComponent<Icono>().AsignarTextura(sprites[4]);
+                iconos[2].GetComponent<Icono>().AsignarTextura(sprites[5]);
+                break;
+            default:
+                break;
+        }
+    }
+    
+    void AsignarCoord()
+    {
+        switch (idPersonaje)
+        {
+            case 0:
+                posy = 4.7f;
+                break;
+            case 1:
+                posy = 4.5f;
+                break;
+            case 2:
+                posy = 4.4f;
+                break;
+            case 3:
+                posy = 4.1f;
+                break;
+            case 4:
+                posy = 4.7f;
+                break;
+            case 5:
+                posy = 4.5f;
+                break;
+            default:
+                break;
+        }
+
+    }
+    void AsignarTexturas()
+    {
+        switch (idPersonaje)
+        {
+            case 0:
+                per.GetComponent<Animator>().SetFloat("personaje", 1f);
+                break;
+            case 1:
+                per.GetComponent<Animator>().SetFloat("personaje", 2f);
+                break;
+            case 2:
+                per.GetComponent<Animator>().SetFloat("personaje", 3f);
+                break;
+            case 3:
+                per.GetComponent<Animator>().SetFloat("personaje", 4f);
+                break;
+            case 4:
+                per.GetComponent<Animator>().SetFloat("personaje", 5f);
+                break;
+            case 5:
+                per.GetComponent<Animator>().SetFloat("personaje", 6f);
+                break;
+            default:
+                break;
+        }      
+
+    }
+
+   
+
+}
---
+++ Assets/Scripts/Habilidades.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Habilidades : MonoBehaviour
+{
+    public GameObject scripts, per;
+    public float tiempo;
+    public bool actTiempo = false;
+    public bool esHab1=false, esHab2=false, esHab3= false, usoHab = false, verCasiHab1Zor=false, condiZor1=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (actTiempo == true) { tiempo = Time.deltaTime + tiempo; }
+        per = GameObject.FindGameObjectWithTag("PerPref");
+        scripts = GameObject.Find("Scripts");
+        terminarHabilidades();
+    }
+
+    public void habilidad1()
+    {
+        
+        esHab1 = true;
+        switch (GetComponent<CrearPersonaje>().idPersonaje)
+        {
+            case 0:
+                hab1zorem();
+                break;
+            case 1:
+                
+                break;
+            case 2:
+               
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            default:
+                break;
+        }
+        per.GetComponent<Personaje>().ph = per.GetComponent<Personaje>().ph - 1;
+        per.GetComponent<Personaje>().imprimePh();
+        usoHab = true;
+    }
+    public void habilidad2()
+    {
+        esHab2 = true;
+        switch (GetComponent<CrearPersonaje>().idPersonaje)
+        {
+            case 0:
+                //hab2zorem();
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            default:
+                break;
+        }
+        per.GetComponent<Personaje>().ph = per.GetComponent<Personaje>().ph - 2;
+        per.GetComponent<Personaje>().imprimePh();
+        usoHab = true;
+    }
+    public void habilidad3()
+    {
+        esHab3 = true;
+        switch (GetComponent<CrearPersonaje>().idPersonaje)
+        {
+            case 0:
+                hab3zorem();
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            default:
+                break;
+        }
+        per.GetComponent<Personaje>().ph = per.GetComponent<Personaje>().ph - 3;
+        per.GetComponent<Personaje>().imprimePh();
+        usoHab = true;
+    }
+   
+
+    public void hab1zorem()
+    {
+            verCasiHab1Zor = true;
+            actTiempo = true;
+            GetComponent<Dado>().esTurno = false;
+            per.GetComponent<Animator>().SetBool("hab1", true);
+    }
+    public void terhab1zor()
+    {
+        
+        if (tiempo >= 2.5f)
+        {
+            
+            if (tiempo >= 2f && condiZor1 == false)
+            {
+                GetComponent<CrearPersonaje>().posx = GetComponent<CrearPersonaje>().posx + 6;
+                per.transform.localPosition = new Vector3(scripts.GetComponent<CrearPersonaje>().posx, scripts.GetComponent<CrearPersonaje>().posy);
+                condiZor1 = true;
+            }
+            else
+            {
+                GetComponent<Dado>().btnHab1.SetActive(false);
+            }
+        }
+        if (tiempo >= 3.5f)
+        {
+           
+            per.GetComponent<Personaje>().casillaActual = per.GetComponent<Personaje>().casillaActual + 3;
+            per.GetComponent<Animator>().SetBool("hab1", false);
+            per.GetComponent<Personaje>().imprimeCasilla();
+            GetComponent<Dado>().esTurno = true;
+            actTiempo = false;
+            tiempo = 0;
+            esHab1 = false;
+        }
+    }
+    public void hab3zorem()
+    {
+        actTiempo = true;
+        GetComponent<Dado>().esTurno = false;
+        per.GetComponent<Animator>().SetBool("hab3", true);
+    }
+    public void terhab3zor()
+    {
+        if (tiempo >= 3.5f)
+        {
+            scripts.GetComponent<Dado>().valorMax = 12;
+            per.GetComponent<Animator>().SetBool("hab3", false);
+            GetComponent<Dado>().esTurno = true;
+            actTiempo = false;
+            tiempo = 0;
+            esHab3 = false;
+        }
+    }
+   
+
+    public void terminarHabilidades()
+    {
+        if (esHab1 == true)
+        {
+            terhab1zor();
+            
+        }
+        if (esHab2 == true)
+        {
+            //terhab2zor();
+            
+        }
+        if (esHab3 == true)
+        {
+            terhab3zor();
+            
+        }
+    }
+}
