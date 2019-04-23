3951c208e99c3d18d60286a53e11601ae8780956
isai galvan alaniz <isaig2015@gmail.com>
2019-04-22 20:57:13 -0500

    MAPA DE DARDOS CREADO, RESPAWN DE DARDOS Y DE GLOBOS CON PUNTAJE, FALTA GRANIZO Y BRISA

---
+++ Assets/Scripts/Dardos/Puntaje.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Puntaje : MonoBehaviour
+{
+    public TextMeshProUGUI textCont;
+    public int puntos;
+
+    private void Update()
+    {
+        actualizarUI();
+    }
+
+    public void actualizarUI()
+    {
+        textCont.text = "PUNTOS:" + puntos;
+    }
+}
